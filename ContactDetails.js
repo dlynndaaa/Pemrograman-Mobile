18d8365738fdc12ad2f8dc4d94c70e52b7c942b8
DELINDA MEGA PUTRI <120640950+dlynndaaa@users.noreply.github.com>
2024-06-30 12:03:10 +0700

    Add files via upload

---
+++ ContactDetails.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, Linking, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function ContactDetail({ route }) {
+  const { contact } = route.params;
+
+  // Fungsi untuk menavigasi ke URL LinkedIn
+  const handleLinkedInPress = () => {
+    Linking.openURL(`https://www.linkedin.com/in/${contact.linkedin}`);
+  };
+
+  // Fungsi untuk menavigasi ke halaman Instagram
+  const handleInstagramPress = () => {
+    Linking.openURL(`https://www.instagram.com/${contact.instagram}`);
+  };
+
+  // Fungsi untuk menavigasi ke aplikasi email
+  const handleEmailPress = () => {
+    Linking.openURL(`mailto:${contact.email}`);
+  };
+
+  // Fungsi untuk menelepon nomor telepon
+  const handlePhonePress = () => {
+    Linking.openURL(`tel:${contact.phone}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <Image source={contact.image} style={styles.img} />
+        <Text style={styles.title}>{contact.name}</Text>
+        <TouchableOpacity style={styles.phoneContainer} onPress={handlePhonePress}>
+          <Text style={styles.phone}>{contact.phone}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.separator}></View>
+      <View style={styles.detailsContainer}>
+        <View style={styles.detailRow}>
+          <Icon name="calendar" size={24} color="#3498db" style={styles.icon} />
+          <Text style={[styles.detail, { textAlign: 'center' }]}>Birthdate: {contact.birthdate}</Text>
+        </View>
+        <View style={styles.detailRow}>
+          <Icon name="map-marker" size={24} color="#3498db" style={styles.icon} />
+          <Text style={[styles.detail, { textAlign: 'center' }]}>Address: {contact.address}</Text>
+        </View>
+        {/* Ikon Email */}
+        <View style={styles.iconRow}>
+          <Icon name="envelope" size={24} color="#3498db" />
+          <Text style={[styles.link, { textAlign: 'center' }]} onPress={handleEmailPress}>
+            {contact.email}
+          </Text>
+        </View>
+        {/* Ikon Instagram */}
+        <View style={styles.iconRow}>
+          <Icon name="instagram" size={24} color="#E1306C" />
+          <Text style={[styles.link, { textAlign: 'center' }]} onPress={handleInstagramPress}>
+            @{contact.instagram}
+          </Text>
+        </View>
+        {/* Ikon LinkedIn */}
+        <View style={styles.iconRow}>
+          <Icon name="linkedin" size={24} color="#0077B5" />
+          <Text style={[styles.link, { textAlign: 'center' }]} onPress={handleLinkedInPress}>
+            {contact.linkedin}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start', // Mengatur konten dari bagian atas layar
+    padding: 20,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  phoneContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  phone: {
+    fontSize: 18,
+    color: '#3498db',
+    marginLeft: 5,
+    textAlign: 'center',
+  },
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#ccc',
+    marginVertical: 10,
+  },
+  detailsContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  detailRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  detail: {
+    fontSize: 16,
+    marginLeft: 10,
+    textAlign: 'center',
+  },
+  iconRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  link: {
+    fontSize: 16,
+    color: 'blue',
+    textDecorationLine: 'underline',
+    marginLeft: 5,
+    textAlign: 'center',
+  },
+});
